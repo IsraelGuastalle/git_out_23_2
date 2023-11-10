fdd190dd9b9571ccf3d8904779630e464c1f2567
Israel Guastalle <israel.guastalle@hotmail.com>
2023-11-10 14:55:55 -0300

    Alterado caminho no arquivo extraction.py

--- src/extraction.py
+++ src/extraction.py
@@ -4,4 +4,5 @@ import streamlit as st
 import xlsxwriter
 
 def load_data():
-    return pd.read_csv('C:/Users/israelguastalle-sao/repos_cds/git_para_ds_out_23/projeto/data/bikes_completed.csv')
+    return pd.read_csv('data/bikes_completed.csv')
+    #return pd.read_csv('C:/Users/israelguastalle-sao/repos_cds/git_para_ds_out_23/projeto/data/bikes_completed.csv'
