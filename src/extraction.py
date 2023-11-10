b2a07d9fe107d933f0bb4e9e30d62d2699eeb9cd
Israel Guastalle <israel.guastalle@hotmail.com>
2023-11-10 11:04:04 -0300

    Correção de bugs e caminhos

--- src/extraction.py
+++ src/extraction.py
@@ -3,4 +3,4 @@ import numpy as np
 import streamlit as st
 
 def load_data():
-    return pd.read_csv('data/processed/bikes_completed.csv')
+    return pd.read_csv('C:/Users/israelguastalle-sao/repos_cds/git_para_ds_out_23/projeto/data/bikes_completed.csv')
